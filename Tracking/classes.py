d70414f3160cc78d439cc24b2a9398d0fc558b02
Stoica Tedy <stoicatedy@gmail.com>
2020-07-01 08:06:54 +0300

    asd

--- Tracking/classes.py
+++ Tracking/classes.py
@@ -16,4 +16,16 @@ class Photo(DB.Model):
         self.url = url
         self.time = int(time.time())
 
+
+class PhotoURL(DB.Model):
+    __tablename__ = "photourl"
+    id = DB.Column(DB.Integer, primary_key=True)
+    url = DB.Column(DB.String(50))
+    data = DB.Column(DB.String(15))
+
+    def __init__(self, url, data):
+        self.url = url
+        self.data = data
+
+
 DB.create_all()
--- Tracking/modules/photo.py
+++ Tracking/modules/photo.py
@@ -1,11 +1,40 @@
 from flask import Blueprint, request, send_file
-from Tracking.classes import Photo
+from Tracking.classes import Photo, PhotoURL
 from Tracking.main import DB, APP
 import io
 import base64
+import uuid
+import json
 
 
 photo_bl = Blueprint("Photo", __name__)
+
+
+@photo_bl.route('/admin/<path>')
+def admin_path(path):
+    query = Photo.query.filter(Photo.url == path).all()
+    result = []
+
+    for photo in query:
+        asd = {
+            "more_data": False,
+            "data": {}
+        }
+        more_data = PhotoURL.query.filter(PhotoURL.url == photo.url).first()
+        if more_data is not None:
+            asd["data"].update(json.loads(more_data.data))
+            asd["more_data"] = True
+        asd.update({
+            "ip": photo.ip,
+            "url": photo.url,
+            "query": str(photo.query_txt),
+            "time": photo.time
+        })
+        result.append(asd)
+    return {
+        "success": True,
+        "result": result
+    }
 
 
 @photo_bl.route('/admin')
@@ -14,23 +43,44 @@ def admin():
     result = []
 
     for photo in query:
-        result.append({
+        asd = {
+            "more_data": False,
+            "data": {}
+        }
+        more_data = PhotoURL.query.filter(PhotoURL.url == photo.url).first()
+        if more_data is not None:
+            asd["data"].update(json.loads(more_data.data))
+            asd["more_data"] = True
+        asd.update({
             "ip": photo.ip,
             "url": photo.url,
             "query": str(photo.query_txt),
             "time": photo.time
         })
+        result.append(asd)
     return {
         "success": True,
         "result": result
     }
 
 
+@photo_bl.route('/create', methods=["POST"])
+def create_url():
+    data = str(request.data.decode('UTF-8'))
+    uuid_str = str(uuid.uuid4())
+    saved = PhotoURL(uuid_str, data)
+    DB.session.add(saved)
+    DB.session.commit()
+    return {
+        "uuid": uuid_str
+    }
+
+
 @photo_bl.route('/', defaults={'path': ''})
 @photo_bl.route('/<path:path>')
 def catch_all(path, **options):
-    query = request.query_string
-    url = request.path
+    query = request.query_string.decode('UTF-8')
+    url = request.path.replace('/photo/', '').replace('.gif', '')
     ip = request.remote_addr
     photo = Photo(ip, url, query)
     DB.session.add(photo)
